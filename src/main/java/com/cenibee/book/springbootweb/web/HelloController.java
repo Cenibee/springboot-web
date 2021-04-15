af5ab21dd53b1e82d257f312ac9848a672ed07f2
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-04-16 01:00:51 +0900

    Ch 2. HelloController

---
+++ src/main/java/com/cenibee/book/springbootweb/web/HelloController.java
@@ -0,0 +1,14 @@
+package com.cenibee.book.springbootweb.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello";
+    }
+
+}
