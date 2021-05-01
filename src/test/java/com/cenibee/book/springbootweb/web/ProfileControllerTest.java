07e4784f918ce11ada08707caeb32c84cc71ff21
Cenibee <30590761+Cenibee@users.noreply.github.com>
2021-05-01 23:00:06 +0900

    Ch. 10. 프로필 조회 - tnwjd

--- src/test/java/com/cenibee/book/springbootweb/web/ProfileControllerTest.java
+++ src/test/java/com/cenibee/book/springbootweb/web/ProfileControllerTest.java
@@ -1,12 +1,11 @@
 package com.cenibee.book.springbootweb.web;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -23,7 +22,7 @@ public class ProfileControllerTest {
     void unauthorizedProfile() throws Exception {
         mvc.perform(get("/profile"))
                 .andExpect(status().isOk())
-                .andExpect(content().string("default"));
+                .andExpect(content().string(Matchers.oneOf("default", "oauth")));
     }
 
 }
